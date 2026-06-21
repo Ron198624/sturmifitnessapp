6d930cc775a12238c8b8a68bd4fee41643c08ac8
Ronny <Ronnyscheffel@hotmail.de>
2026-06-21 13:15:14 +0200

    Design3.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
     <nav className="w-full bg-black/90 backdrop-blur-xl border-b border-gray-700 
-                    text-white py-5 mb-10">
-      <ul className="flex justify-between max-w-3xl mx-auto w-full px-4 text-2xl font-semibold">
+                    text-white py-4 mt-6 mb-10">
+      <ul className="flex justify-center gap-10 text-2xl font-semibold">
         <li><a href="/" className="hover:text-[#00ff9d] transition">Home</a></li>
         <li><a href="/training" className="hover:text-[#00ff9d] transition">Training</a></li>
         <li><a href="/cardio" className="hover:text-[#00ff9d] transition">Cardio</a></li>
